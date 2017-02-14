e2610d1796c2147aa26b9cb2a0b907258dfd9216
ashwin37 <ashwint379@gmail.com>
2017-02-14 12:02:22 -0500

    MyJSON
    Sample JSON to display Name and Age of a person in TableView using
    third party SwiftyJSON, Which is a better way to deal with JSON data in
    Swift !!

--- MyJSON/ViewController.swift
+++ MyJSON/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var TBV: UITableView!
+    
+    var numOfRows = 0
+    
+    var namesArray = [String]() // An Array
+    var ageArray = [String]()   // An Array
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        TBV.dataSource = self
+        TBV.delegate = self
+        
+        parseJSON()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func parseJSON()
+    {
+        let path : String = Bundle.main.path(forResource: "Data", ofType: "json")!
+        
+        let jsonData = NSData(contentsOfFile: path) as NSData!
+        
+       let readableJSON = JSON(data: jsonData as! Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+        
+        let Name = readableJSON["People"] ["Person1"] ["Age"]
+        
+        numOfRows = readableJSON["People"].count
+        
+        for i in 1...numOfRows
+        {
+            var Person = "Person"
+            Person += "\(i)"
+            let Name = readableJSON["People"][Person]["Name"].string as String!
+            let Age = readableJSON["People"][Person]["Age"].string as String!
+         
+            namesArray.append(Name!)
+            ageArray.append(Age!)
+            print("\(namesArray)", "\(ageArray)")
+            
+        }
+        print("\(Name)")
     }
-
-
-}
-
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return numOfRows
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+       if namesArray.count != 0
+       {
+        cell.textLabel?.text = namesArray[indexPath.row]
+       }
+        
+    return cell
+        
+    }
+   }
---
+++ Pods/Target Support Files/Pods-MyJSON/Pods-MyJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyJSON : NSObject
+@end
+@implementation PodsDummy_Pods_MyJSON
+@end
